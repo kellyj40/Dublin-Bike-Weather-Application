ff52c563964d482ff0ffdd807de6eaec32c7258f
Joe <kellyj40@tcd.ie>
2017-03-20 15:39:41 +0000

    Locations mysql call

---
+++ flask/data_request.py
@@ -0,0 +1,13 @@
+import pymysql
+
+def get_locations():
+    conn = pymysql.connect(host='dublinbikes.clbms7pd8xjt.us-west-2.rds.amazonaws.com', user='goodchat',
+                           password='goodchat', db='DublinBikes')
+    cur = conn.cursor()
+    cur.execute("""SELECT number, name, lat, lng FROM Bike_Data GROUP BY number;""")
+    conn.commit()
+    location_info = []
+    for row in cur.fetchall():
+        location_info.append(row[1])
+    cur.close()
+    return location_info
--- flask/server.py
+++ flask/server.py
@@ -1,10 +1,14 @@
+#Server.py
 from flask import Flask, render_template
-
+from data_request import *
 app = Flask(__name__)
+
+
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    locations = get_locations()
+    return render_template('index.html', locations=locations)
 
 if __name__ == "__main__":
     app.run(debug=True)
