f92cb69e4cddb7e5b295dce4ba859edd4d6daf6d
Joe <kellyj40@tcd.ie>
2017-03-20 18:56:13 +0000

    dropdown form

--- flask/server.py
+++ flask/server.py
@@ -1,14 +1,19 @@
 #Server.py
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from data_request import *
 app = Flask(__name__)
-
-
 
 @app.route('/')
 def index():
     locations = get_locations()
     return render_template('index.html', locations=locations)
+
+@app.route('/location')
+def location_selection():
+    path = request.url
+    useless,number = path.split("=")
+    return render_template('dubikes.html', number=number)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
