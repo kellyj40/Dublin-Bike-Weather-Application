6dd91fd6447fb92ecb3c4cc9963c92758dd3fd55
cojowy <16202597@ucdconnect.ie>
2017-03-20 16:49:17 +0000

    markers on map, unused files deleted

--- flask/static/js/map.js
+++
@@ -1,8 +0,0 @@
-function googleMap()
-{
-    var canvas= document.getElementById("home_map");
-    var myCenter= new google.maps.LatLng(53.355122,-6.24922)
-    var options= {center: myCenter, zoom:13}
-    var map= new google.maps.Map(canvas,options);
-    var mapLabel=new google.maps.InfoWindow({position: new google.maps.LatLng(53.356, -6.26)});
-}
