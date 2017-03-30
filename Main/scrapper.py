19b12b5282bb063bc03cf4d80c2445c21c17766c
Jake Kavanagh <Jake Kavanagh>
2017-03-30 17:50:18 +0100

    improved sql statments, and multiple tables

--- Main/scrapper.py
+++ Main/scrapper.py
@@ -32,22 +32,37 @@ def store(json_data):
 
 
 def connect(conn, json_data):
+    cur = conn.cursor()
+    try:
+        cur.execute("""CREATE TABLE IF NOT EXISTS `DublinBikes`.`Bike_Data`(`number` INT NOT NULL,`banking` VARCHAR(45),
+        `bonus` VARCHAR(45),`status` VARCHAR(45),`contract_name` VARCHAR(45),`bike_stands` INT(11),`available_bike_stands`
+         INT(11),`available_bikes` INT(11),`last_update` BIGINT(20), PRIMARY KEY (`number`,`last_update`));""")
+        conn.commit()
+        cur.execute("""CREATE TABLE IF NOT EXISTS `DublinBikes`.`Static_Data`(`number` INT NOT NULL,`name` VARCHAR(45),
+                `address` VARCHAR(45),`lat` VARCHAR(45),`lng` VARCHAR(45), PRIMARY KEY (`number`));""")
+        conn.commit()
+    except:
+        print("there is no error joe")
+        pass
     for i in range(0, len(json_data)):
-        cur = conn.cursor()
-        query = """INSERT INTO Bike_Data(number, name, address, lat, lng, banking, bonus, status, contract_name, bike_stands, available_bike_stands, available_bikes, last_update) VALUES ("%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s");"""
+        query = """INSERT INTO Bike_Data(number, banking, bonus, status, contract_name, bike_stands, available_bike_stands,
+         available_bikes, last_update) VALUES ("%s","%s","%s","%s","%s","%s","%s","%s","%s");"""
+        query2 = """INSERT INTO Static_Data(number, name, address, lat, lng) VALUES ("%s","%s","%s","%s","%s");"""
         try:
-            cur.execute(query % (json_data[i]['number'], json_data[i]['name'], json_data[i]['address'],
-                                 json_data[i]['position']['lat'], json_data[i]['position']['lng'],
+            cur.execute(query % (json_data[i]['number'],
                                  json_data[i]['banking'],
                                  json_data[i]['bonus'], json_data[i]['status'], json_data[i]['contract_name'],
                                  json_data[i]['bike_stands'], json_data[i]['available_bike_stands'],
                                  json_data[i]['available_bikes'], json_data[i]['last_update']))
-
+            conn.commit()
+            cur.execute(query2 % (json_data[i]['number'],
+                                 json_data[i]['name'],
+                                 json_data[i]['address'], json_data[i]['position']['lat'], json_data[i]['position']['lng']))
+            conn.commit()
         except pymysql.err.IntegrityError as e:
             continue
         except:
             file = open('errors_store.txt', 'a')
             file.write(str(sys.exc_info()[0]) + '\n')
             file.close()
-        conn.commit()
 main()
