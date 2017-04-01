805593db85c0a3ba03c75b8226e84e30cc7f0049
Joe <kellyj40@tcd.ie>
2017-04-01 13:10:56 +0100

    Only one connection need, object oriented layout

--- flask/data_request.py
+++ flask/data_request.py
@@ -1,62 +1,55 @@
 import pymysql
+class database_queries:
+    def __init__(self):
+        self.conn = pymysql.connect(host='dublinbikes.clbms7pd8xjt.us-west-2.rds.amazonaws.com', user='goodchat',
+                               password='goodchat', db='DublinBikes')
 
-def test_function():
-    return "hello"
+    def get_locations(self):
+        cur = self.conn.cursor()
+        cur.execute("""SELECT number, name, lat, lng FROM Static_Data;""")
+        self.conn.commit()
+        location_info = []
+        for row in cur.fetchall():
+            location_info.append(row)
+        cur.close()
+        return location_info
 
-def get_locations():
-    conn = pymysql.connect(host='dublinbikes.clbms7pd8xjt.us-west-2.rds.amazonaws.com', user='goodchat',
-                           password='goodchat', db='DublinBikes')
-    cur = conn.cursor()
-    cur.execute("""SELECT number, name, lat, lng FROM Static_Data;""")
-    conn.commit()
-    location_info = []
-    for row in cur.fetchall():
-        location_info.append(row)
-    cur.close()
-    return location_info
+    def get_info(self, val):
+        cur = self.conn.cursor()
+        query_string = "SELECT name FROM Static_Data WHERE number =  '{val}'".format(val=val)
+        cur.execute(query_string)
+        self.conn.commit()
+        for row in cur.fetchall():
+            place = row
+            break
 
-def get_info(val):
-    conn = pymysql.connect(host='dublinbikes.clbms7pd8xjt.us-west-2.rds.amazonaws.com', user='goodchat',
-                           password='goodchat', db='DublinBikes')
-    cur = conn.cursor()
-    query_string = "SELECT name FROM Static_Data WHERE number =  '{val}'".format(val=val)
-    cur.execute(query_string)
-    conn.commit()
-    for row in cur.fetchall():
-        place = row
-        break
+        query_string = "SELECT * FROM Bike_Data WHERE number =  '{val}'".format(val=val)
+        cur.execute(query_string)
+        self.conn.commit()
+        information = []
+        for row in cur.fetchall():
+            information.append(row)
 
-    query_string = "SELECT * FROM Bike_Data WHERE number =  '{val}'".format(val=val)
-    cur.execute(query_string)
-    conn.commit()
-    information = []
-    for row in cur.fetchall():
-        information.append(row)
+        cur.close()
+        return place, information
 
-    cur.close()
-    return place, information
+    def get_station_name(self, station_number):
+        cur = self.conn.cursor()
+        query_string = "SELECT name FROM Static_Data WHERE number =  '{val}'".format(val=station_number)
+        cur.execute(query_string)
+        self.conn.commit()
+        for row in cur.fetchall():
+            place = row
+            break
+        return place
 
-def get_station_name(station_number):
-    conn = pymysql.connect(host='dublinbikes.clbms7pd8xjt.us-west-2.rds.amazonaws.com', user='goodchat',
-                           password='goodchat', db='DublinBikes')
-    cur = conn.cursor()
-    query_string = "SELECT name FROM Static_Data WHERE number =  '{val}'".format(val=station_number)
-    cur.execute(query_string)
-    conn.commit()
-    for row in cur.fetchall():
-        place = row
-        break
-    return place
-
-def current_info():
-    conn = pymysql.connect(host='dublinbikes.clbms7pd8xjt.us-west-2.rds.amazonaws.com', user='goodchat',
-                           password='goodchat', db='DublinBikes')
-    cur = conn.cursor()
-    query_string = "SELECT * FROM (SELECT * FROM DublinBikes.Bike_Data ORDER BY last_update DESC) as mytable GROUP BY mytable.number;"
-    cur.execute(query_string)
-    conn.commit()
-    data={} #creating python dictionary to store the data for each stop number
-    for row in cur.fetchall():
-        data[str(row[0])]=row[1:]
-    cur.close()
-    return data
+    def current_info(self):
+        cur = self.conn.cursor()
+        query_string = "SELECT * FROM (SELECT * FROM DublinBikes.Bike_Data ORDER BY last_update DESC) as mytable GROUP BY mytable.number;"
+        cur.execute(query_string)
+        self.conn.commit()
+        data={} #creating python dictionary to store the data for each stop number
+        for row in cur.fetchall():
+            data[str(row[0])]=row[1:]
+        cur.close()
+        return data
--- flask/server.py
+++ flask/server.py
@@ -5,17 +5,19 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    locations = get_locations()
-    current_data = current_info()
+    db_queries_home = database_queries()
+    locations = db_queries_home.get_locations()
+    current_data = db_queries_home.current_info()
     return render_template('index.html', locations=locations, current_data=current_data)
 
 
 @app.route('/location')
 def location_selection():
+    db_queries_locations = database_queries()
     path = request.url
     useless, number_of_station = path.split("=")
-    name_of_place = get_station_name(number_of_station)
-    most_recent_data = current_info()
+    name_of_place = db_queries_locations.get_station_name(number_of_station)
+    most_recent_data = db_queries_locations.current_info()
     all_data=most_recent_data[number_of_station]
     return render_template('dubikes.html', name_of_place=name_of_place, all_data=all_data)
 
