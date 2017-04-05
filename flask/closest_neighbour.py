d10fc8a6e570f95787e3f4da0854d3211f5a625c
Joe <kellyj40@tcd.ie>
2017-04-05 09:46:58 +0100

    The closest 3 stations to selected stattion and links

--- flask/closest_neighbour.py
+++ flask/closest_neighbour.py
@@ -6,7 +6,10 @@ def rad(x):
 
 def find_closest_neighbours(place, neighbours):
     radius = 6378137 #Earths radius
-    closest = 10000
+    details_neighbour = [neighbours[0], neighbours[1], neighbours[2]]
+    closest1 = 10000
+    closest2 = 10001
+    closest3 = 10002
     for location in neighbours:
         if location[0] != place[0][0]: #dont let calculate distance with itself
             dLat = rad(float(place[0][2]) - float(location[2]))  # getting the latitudes deference in rads
@@ -14,10 +17,21 @@ def find_closest_neighbours(place, neighbours):
             a = math.sin(dLat / 2) * math.sin(dLat / 2) + math.cos(rad(float(location[2]))) * math.cos(rad(float(place[0][2]))) * math.sin(dLong / 2) * math.sin(dLong / 2)
             c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
             d = radius * c
-            if d < closest: #If its closer than current closest, then change out
-                closest = d
-                details_neighbour = location
-    print(details_neighbour)
+            if d < closest1: #If its closer than current closest, then change out
+                closest3 = closest2
+                closest2 = closest1
+                closest1 = d
+                details_neighbour[2] = details_neighbour[1]
+                details_neighbour[1] = details_neighbour[0]
+                details_neighbour[0] = location
+            elif d<closest2:
+                closest3 = closest2
+                closest2 = d
+                details_neighbour[2] = details_neighbour[1]
+                details_neighbour[1] = location
+            elif d<closest3:
+                closest3 = d
+                details_neighbour[2] = location
     return details_neighbour
 
 #####Formula taken from http://stackoverflow.com/questions/1502590/calculate-distance-between-two-points-in-google-maps-v3
