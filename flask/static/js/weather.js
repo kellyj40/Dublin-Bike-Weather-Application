e1915c76191733f58bbc365ee281bdd1c9cc315a
cojowy <16202597@ucdconnect.ie>
2017-04-20 11:13:13 +0100

    Update to weather box

--- flask/static/js/weather.js
+++ flask/static/js/weather.js
@@ -1,8 +1,8 @@
  function weather(x){
     var icon = "<img src='http://openweathermap.org/img/w/"+x['Dublin'][3]+".png'>";
     var temp = parseInt(x['Dublin'][0]- 273.15);
-    var weather = icon + "<br> "+ x['Dublin'][2] + "<br>Temperature: " + temp +
-    "<sup>o</sup>C<br> Windspeed: "+x['Dublin'][1] +" m/s<br> Last Update: "+x['Dublin'][4]+" minutes ago";
+    var weather = "<table><tr><td>Current Weather</td><td id='weather_icon_cell'>"+icon+"</td></tr><tr><td>Temperature: </td><td>" + temp +
+    "<sup>o</sup>C</td></tr><tr><td>Last Update: </td><td>"+x['Dublin'][4]+" minutes ago</td></tr><table>";
 
     document.getElementById("weather").innerHTML = weather;
 }
